7c18b5dd6f381f4895ccfd648fb8fdd4977c0101
Jeremiah Umoh <umohjeremiah43@gmail.com>
2026-05-19 05:41:44 +0100

    first  github attempt

--- index.php
+++ index.php
@@ -47,27 +47,7 @@
               <div class="col-sm-12">
                 <div class="home-tab">
                   <div class="d-sm-flex align-items-center justify-content-between border-bottom">
-                    <ul class="nav nav-tabs" role="tablist">
-                      <li class="nav-item">
-                        <a class="nav-link active ps-0" id="home-tab" data-bs-toggle="tab" href="#overview" role="tab" aria-controls="overview" aria-selected="true">Overview</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#audiences" role="tab" aria-selected="false">Audiences</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#demographics" role="tab" aria-selected="false">Demographics</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link border-0" id="more-tab" data-bs-toggle="tab" href="#more" role="tab" aria-selected="false">More</a>
-                      </li>
-                    </ul>
-                    <div>
-                      <div class="btn-wrapper">
-                        <a href="#" class="btn btn-otline-dark align-items-center"><i class="icon-share"></i> Share</a>
-                        <a href="#" class="btn btn-otline-dark"><i class="icon-printer"></i> Print</a>
-                        <a href="#" class="btn btn-primary text-white me-0"><i class="icon-download"></i> Export</a>
-                      </div>
-                    </div>
+                 
                   </div>
                   <div class="tab-content tab-content-basic">
                     <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview">
@@ -363,74 +343,6 @@
                           </div>
                         </div>
                         <div class="col-lg-4 d-flex flex-column">
-                          <div class="row flex-grow">
-                            <div class="col-12 grid-margin stretch-card">
-                              <div class="card card-rounded">
-                                <div class="card-body">
-                                  <div class="row">
-                                    <div class="col-lg-12">
-                                      <div class="d-flex justify-content-between align-items-center">
-                                        <h4 class="card-title card-title-dash">Todo list</h4>
-                                        <div class="add-items d-flex mb-0">
-                                          <!-- <input type="text" class="form-control todo-list-input" placeholder="What do you need to do today?"> -->
-                                          <button class="add btn btn-icons btn-rounded btn-primary todo-list-add-btn text-white me-0 pl-12p"><i class="mdi mdi-plus"></i></button>
-                                        </div>
-                                      </div>
-                                      <div class="list-wrapper">
-                                        <ul class="todo-list todo-list-rounded">
-                                          <li class="d-block">
-                                            <div class="form-check w-100">
-                                              <label class="form-check-label">
-                                                <input class="checkbox" type="checkbox"> Lorem Ipsum is simply dummy text of the printing <i class="input-helper rounded"></i>
-                                              </label>
-                                              <div class="d-flex mt-2">
-                                                <div class="ps-4 text-small me-3">24 June 2020</div>
-                                                <div class="badge badge-opacity-warning me-3">Due tomorrow</div>
-                                                <i class="mdi mdi-flag ms-2 flag-color"></i>
-                                              </div>
-                                            </div>
-                                          </li>
-                                          <li class="d-block">
-                                            <div class="form-check w-100">
-                                              <label class="form-check-label">
-                                                <input class="checkbox" type="checkbox"> Lorem Ipsum is simply dummy text of the printing <i class="input-helper rounded"></i>
-                                              </label>
-                                              <div class="d-flex mt-2">
-                                                <div class="ps-4 text-small me-3">23 June 2020</div>
-                                                <div class="badge badge-opacity-success me-3">Done</div>
-                                              </div>
-                                            </div>
-                                          </li>
-                                          <li>
-                                            <div class="form-check w-100">
-                                              <label class="form-check-label">
-                                                <input class="checkbox" type="checkbox"> Lorem Ipsum is simply dummy text of the printing <i class="input-helper rounded"></i>
-                                              </label>
-                                              <div class="d-flex mt-2">
-                                                <div class="ps-4 text-small me-3">24 June 2020</div>
-                                                <div class="badge badge-opacity-success me-3">Done</div>
-                                              </div>
-                                            </div>
-                                          </li>
-                                          <li class="border-bottom-0">
-                                            <div class="form-check w-100">
-                                              <label class="form-check-label">
-                                                <input class="checkbox" type="checkbox"> Lorem Ipsum is simply dummy text of the printing <i class="input-helper rounded"></i>
-                                              </label>
-                                              <div class="d-flex mt-2">
-                                                <div class="ps-4 text-small me-3">24 June 2020</div>
-                                                <div class="badge badge-opacity-danger me-3">Expired</div>
-                                              </div>
-                                            </div>
-                                          </li>
-                                        </ul>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
                           <div class="row flex-grow">
                             <div class="col-12 grid-margin stretch-card">
                               <div class="card card-rounded">
--- partials/sidebar.php
+++ partials/sidebar.php
@@ -5,18 +5,17 @@
                 <span class="menu-title">Dashboard</span>
               </a>
             </li>
-            <li class="nav-item nav-category">UI Elements</li>
             <li class="nav-item">
               <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <i class="menu-icon mdi mdi-floor-plan"></i>
-                <span class="menu-title">UI Elements</span>
+                <i class="menu-icon mdi mdi-account"></i>
+                <span class="menu-title">Accounts</span>
                 <i class="menu-arrow"></i>
               </a>
               <div class="collapse" id="ui-basic">
                 <ul class="nav flex-column sub-menu">
-                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
-                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="Admin/admin_login.php">Admin Login</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="Admin/Register_students.php">Register students</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="Admin/Change_password.php">Change password</a></li>
                 </ul>
               </div>
             </li>
